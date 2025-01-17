0a6ad6e64ae027254fb049cce58c6175abefd8ef
gokul170923 <145271997+gokul170923@users.noreply.github.com>
2025-01-18 00:01:41 +0530

    done

--- algoLogic.js
+++ algoLogic.js
@@ -8,7 +8,11 @@ const SortingName = {
         "BubbleSort":BubbleSort,
         "SelectionSort":SelectionSort,
         "InsertionSort":InsertionSort,
+        "MergeSort":MergeSort,
+        "QuickSort":QuickSort,
 };
+
+
 
 
 async function BubbleSort(){
@@ -43,6 +47,10 @@ async function BubbleSort(){
         }
 }
 
+
+
+
+
 async function SelectionSort(){
         let n = array.length;
         for(let i=0;i<n;i++){
@@ -73,6 +81,10 @@ async function SelectionSort(){
         }
 }
 
+
+
+
+
 async function InsertionSort(){
         let n = array.length;
         for(let i=0;i<n;i++){
@@ -101,6 +113,125 @@ async function InsertionSort(){
 
 
 
+
+
+
+
+async function merge( l,mid,r){
+        n1 = mid-l+1 , n2 = r-mid;
+        a1 = [] , a2 = [];
+        for(let i=0;i<n1;i++){
+                a1[i]=array[i+l];
+                MainBox.childNodes[i + l].style.backgroundColor = red;
+        }
+        for(let i=0;i<n2;i++){
+                a2[i] = array[i+mid+1];
+                MainBox.childNodes[i + mid+1].style.backgroundColor = purple;
+        }
+        let j=0,k=0;
+        for(let i=l;i<=r;i++){
+                await delay();
+                if( j <n1 && k<n2){
+                        if(a1[j]<a2[k]){
+                                array[i] = a1[j];
+                                MainBox.childNodes[i].style.height = `${a1[j]}px`;
+                                j++;
+                        }else{
+                                array[i]=a2[k];
+                                MainBox.childNodes[i].style.height = `${a2[k]}px`;
+                                k++;
+                        }
+                }
+                else if(j<n1){
+                        array[i] = a1[j];
+                        MainBox.childNodes[i].style.height = `${a1[j]}px`;
+                        j++;
+                }
+                else{
+                        array[i]=a2[k];
+                        MainBox.childNodes[i].style.height = `${a2[k]}px`;
+                        k++;
+                }
+                swapSound();
+        }
+        for(let i=l;i<=r;i++){
+                MainBox.childNodes[i].style.backgroundColor = green;
+        }
+        CompleteSound();
+}
+
+async function mergesort(l , r){
+        if(l>=r) return;
+        let mid = Math.floor((l+r)/2);
+        await mergesort(l,mid);
+        await mergesort(mid+1,r);
+        await merge(l,mid,r);
+
+}
+
+async function MergeSort(){
+        let n = array.length;
+        await mergesort(0,n-1);     
+}
+
+
+
+
+
+
+
+async function partison(l,r){
+        MainBox.childNodes[r].style.backgroundColor = red;
+        let start = l,end = r-1;
+        while(start<=end){
+                MainBox.childNodes[start].style.backgroundColor = purple;
+                MainBox.childNodes[end].style.backgroundColor = purple;
+                await delay();
+                if(array[start]<array[r]){
+                        MainBox.childNodes[start].style.backgroundColor = pink;
+                        start++;
+                }
+                else{
+                        let temp = array[end];
+                        array[end]=array[start];
+                        array[start]= temp;
+                        swapSound();
+
+                        MainBox.childNodes[end].style.height = `${array[end]}px`;
+                        MainBox.childNodes[start].style.height = `${array[start]}px`;
+
+                        MainBox.childNodes[end].style.backgroundColor = pink;
+                        end--;
+                }
+    
+        }
+
+        let temp =array[r];
+        array[r] = array[start];
+        array[start]= temp;
+
+        MainBox.childNodes[r].style.height = `${array[r]}px`;
+        MainBox.childNodes[start].style.height = `${array[start]}px`;
+
+        MainBox.childNodes[r].style.backgroundColor = pink;
+        MainBox.childNodes[start].style.backgroundColor = green;
+        CompleteSound();
+        return start;
+}
+    
+    
+async function quicksort(l, r){
+        if(l>r) return;
+        let mid  = await partison(l,r);
+        await quicksort(l,mid-1);
+        await quicksort(mid+1,r);
+    
+}
+    
+async function QuickSort(){
+        let n = array.length;
+        await quicksort(0,n-1);
+}
 
 
 
